533187f968936da53b564dee86309e62146bf7be
Marcin Kwiatkowski <marcinkwiatkowski04@gmail.com>
2022-03-10 13:27:49 +0100

    trying to display data in frontend

--- ClientApp/bench/src/app/chart/chart.component.ts
+++ ClientApp/bench/src/app/chart/chart.component.ts
@@ -1,18 +1,39 @@
-import { Component } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Tick } from '../models/tick';
+import { TickService } from '../tick.service';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
-  styleUrls: ['./chart.component.scss']
+  styleUrls: ['./chart.component.scss'],
 })
-export class ChartComponent {
+export class ChartComponent implements OnInit {
+  constructor(private tickService: TickService) {}
+
+  seriesX: Array<number> = [];
+  seriesY: Array<string> = [];
+
+  ngOnInit(): void {
+    this.getData(this.seriesX, this.seriesY);
+    console.log(this.seriesX);
+    console.log(this.seriesY);
+  }
+
+  getData(seriesX: Array<number>, seriesY: Array<string>) {
+    this.tickService.getTicks().forEach(function (i: Tick): void {
+      seriesX.push(i.getValue);
+      seriesY.push(i.getDate);
+    });
+  }
+
   public chartType: string = 'line';
 
   public chartDatasets: Array<any> = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'My First dataset' }
+    { data: this.seriesX, label: 'Google chart' },
   ];
 
-  public chartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public chartLabels: Array<any> = [this.seriesY];
 
   public chartColors: Array<any> = [
     {
@@ -24,12 +45,12 @@ export class ChartComponent {
       backgroundColor: 'rgba(0, 137, 132, .2)',
       borderColor: 'rgba(0, 10, 130, .7)',
       borderWidth: 2,
-    }
+    },
   ];
 
   public chartOptions: any = {
-    responsive: true
+    responsive: true,
   };
-  public chartClicked(e: any): void { }
-  public chartHovered(e: any): void { }
+  public chartClicked(e: any): void {}
+  public chartHovered(e: any): void {}
 }
--- ClientApp/bench/src/app/tick.service.ts
+++ ClientApp/bench/src/app/tick.service.ts
@@ -12,10 +12,12 @@ export class TickService {
   constructor(private http:HttpClient) { }
 
   //getTicks(startDate: Date, endDate: Date): Observable<Tick[]>{
-  getTicks(): Observable<Tick[]>{
+  getTicks(): Observable<Tick>{
+  //getTicks(){
 
    // const params = { params: new HttpParams({fromString: "_page=1&_limit=10"}) };
    //  return this.http.get<Tick[]>("https://localhost:44377/ticker", params);    
-    return this.http.get<Tick[]>("https://localhost:44377/Ticker?startDate=2019-01-02%2016%3A00%3A00&endDate=2019-01-09%2016%3A00%3A00");    
+    return this.http.get<Tick>("https://localhost:44377/Ticker?startDate=2019-01-02%2016%3A00%3A00&endDate=2019-01-09%2016%3A00%3A00");    
+    //return this.http.get("https://localhost:44377/Ticker?startDate=2019-01-02%2016%3A00%3A00&endDate=2019-01-09%2016%3A00%3A00");    
   }
 }
